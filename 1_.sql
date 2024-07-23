4747254e2769deeb9a28dc3791b8b1972dd76b8b
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-07-23 15:25:38 +0300

    fix: add a id, and created_at, updated_at

--- 1_.sql
+++ 1_.sql
@@ -1,7 +1,10 @@
 CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
     username VARCHAR(100),
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(100),
     password VARCHAR(100)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
