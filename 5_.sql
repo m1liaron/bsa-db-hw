a12ea914d086543e966821def664efcf206ba863
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-07-24 15:27:12 +0300

    feat: get movie by the criterias

--- 5_.sql
+++ 5_.sql
@@ -1,19 +1,19 @@
-SELECT 
-	m.id AS ID
-	m.title as 'Title'
-	m.realease_date as 'Release date'
-	m.duration as 'Duration'
-	m.description as 'Description'
-	m.poster as 'Poster'
-	m.director as 'Director'
-FROM 
-	moivie m
+SELECT
+    m.id AS ID,
+    m.title AS Title,
+    m.release_date AS "Release date",
+    m.duration AS Duration,
+    m.description AS Description,
+    m.poster AS Poster,
+    m.director AS Director
+FROM
+	movie m
 WHERE
 	m.country = 'USA'
-	AND m.realease_date >= '2022-01-01'
-	AND EXTRACT(EPOCH FROM m.duration) > EXTRACT(EPOCH FROM INTERVAL '2 hours 15 minutes'])
-	ADD EXISTS(
-		SELECT 1
-		FROM jsonb_array_elements(m.genres) AS gengre
-		WHERE genre->>'name' IN ('Action', 'Drama')
-	)
+	AND m.release_date >= '2022-01-01'
+	 AND EXTRACT(EPOCH FROM m.duration) > EXTRACT(EPOCH FROM INTERVAL '2 hours 15 minutes')
+    AND EXISTS (
+        SELECT 1
+        FROM jsonb_array_elements(m.genres) AS genre
+        WHERE genre->>'name' IN ('Action', 'Drama')
+    );
