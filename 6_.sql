8a9a6364c3bb5c34e34b5a2094445b47264cfe48
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-07-24 16:41:49 +0300

    feat: get movie with id 1

--- 6_.sql
+++ 6_.sql
@@ -1,9 +1,14 @@
-CREATE TABLE favorite_movies (
-	user_id INT,
-	movie_id INT,
-	PRIMARY KEY (user_id, movie_id),
-	FOREIGN KEY (user_id) REFERENCES "user"(id),
-	FOREIGN KEY (movie_id) REFERENCES movie(id),
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+SELECT
+    m.id AS ID,
+    m.title AS Title,
+    m.release_date AS "Release date",
+    m.duration AS Duration,
+    m.description AS Description,
+    m.poster AS Poster,
+    m.director AS Director,
+    m.actors AS Actors,
+    m.genres AS Genres
+FROM
+    movie m
+WHERE
+    m.id = 1;
